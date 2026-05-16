3e2f000878c71fadb4bccab6770eb504c5580bd5
Comunicando Igrejas <comunicandoigrejas@gmail.com>
2026-05-15 22:35:54 -0300

    Update 4_📱_Auto_Cadastro.py

--- pages/4_📱_Auto_Cadastro.py
+++ pages/4_📱_Auto_Cadastro.py
@@ -41,7 +41,7 @@ st.markdown("""
 st.markdown('<div class="header-box"><h2>⛪ BEM-VINDO À ISOSED</h2><p>Preencha seus dados para atualizar seu cadastro na congregação.</p></div>', unsafe_allow_html=True)
 
 # URL DO SEU GOOGLE APPS SCRIPT (Certifique-se de que é a última gerada como 'Nova Versão')
-WEBAPP_URL = "https://script.google.com/macros/s/AKfycbx6WIrsZbmyGertzhcmNiwotRKWlfHsSi__FnKnr94rpgvQJOFM0HhDjr-TeezUXr1W/exec"
+WEBAPP_URL = "https://script.google.com/macros/s/AKfycbxZySFFLfmq8c3W7QnlmPp8kMCYTix8_Q7zawN13iMAehv1SVJHCwBNPXGUQ6PK7JPn/exec"
 
 # Memória temporária para o endereço (Impede que o endereço suma ao digitar outras coisas)
 if "endereco_auto" not in st.session_state:
